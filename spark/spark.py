e1faf0c19eea15dbca0d8a1a430a8dfb6de3e24e
leonhardt <lariscusObscurus@gmail.com>
2018-06-10 03:57:56 +0200

    Dump to db

--- spark/spark.py
+++ spark/spark.py
@@ -1,20 +1,35 @@
 from __future__ import print_function
 
 import sys
+import psycopg2
 
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.flume import FlumeUtils
 
-def test(x):
-    print("IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII")
-    print("x")
-    print(x)
-    print("x0")
-    print(x[0])
-    print("x1")
-    print(x[1])
-    return x[1];
+
+def test(pair):
+    result = {
+        'product_id': pair[0],
+        'revenue': 0.0,
+    }
+    for it in pair[1]:
+        result['revenue'] += float(it['revenue'])
+    print(result)
+
+    conn = psycopg2.connect(dbname='pgdb', user='pguser', password='pguser', host='192.168.0.15')
+    cur = conn.cursor()
+
+    # Hello SQL injection 
+    cur.execute("INSERT INTO event_statistics (product_id, views, purchases, revenue, timestamp) VALUES (%s, 1, 1, %s, NOW())", (result['product_id'], result['revenue']))
+    conn.commit()
+    conn.close()
+    return result;
+
+def process(rdd):
+    result = rdd.groupBy(lambda rdd: rdd['product_id']).map(test)
+    result.collect()
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -29,11 +44,7 @@ if __name__ == "__main__":
     hostname, port = sys.argv[1:]
     kvs = FlumeUtils.createStream(ssc, hostname, int(port))
 
-    lines = kvs.map(test)
-    counts = lines.flatMap(lambda line: line.split(" ")) \
-       .map(lambda word: (word, 1)) \
-       .reduceByKey(lambda a, b: a+b)
-    counts.pprint()
+    kvs.map(lambda x: x[0]).window(60, 10).foreachRDD(process)
 
     ssc.start()
 ssc.awaitTermination()
